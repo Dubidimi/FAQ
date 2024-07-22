803e4f6efd12a3ddef1daaf50760d4d620826e9b
Dimitri V <dvardidze15@gmail.com>
2024-07-22 18:33:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function openPopup() {
+    var text = document.getElementById("popup");
+    var open = document.getElementById("openbtn");
+    var close = document.getElementById("closebtn");
+        text.style.display = "block"
+        open.style.display = "none"
+        close.style.display = "block"
+    }
+
+
+    function closePopup() {
+    var text = document.getElementById("popup");
+    var open = document.getElementById("openbtn");
+    var close = document.getElementById("closebtn");
+        text.style.display = "none"
+        open.style.display = "block"
+        close.style.display = "none"
+    }
